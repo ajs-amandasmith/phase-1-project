7952a99c26bdfa7f2547531143a608f73de3fbe3
Amanda Smith <ajs.amandasmith@gmail.com>
2022-04-29 15:42:42 -0500

    Updating handlePokeSearch to include a fetch request

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
   submitPokeSearch();
 })
 
-function getPokeData() {
-  fetch('https://pokeapi.co/api/v2/pokemon/')
+function getPokeData(pokemon) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
   .then(res => res.json())
-  .then(data => console.log(data))
+  .then(data => {
+    console.log(data)
+    return pokemon;
+  })
 }
 
 function submitPokeSearch() {
@@ -16,8 +19,17 @@ function submitPokeSearch() {
 function handlePokeSearch(event) {
   event.preventDefault();
   const searchInput = document.getElementById('search');
-  console.log(searchInput.value);
-  console.log(event);
+  const pokemon = searchInput.value;
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+  .then(res => res.json())
+  .then(data => {
+    console.log('data', data);
+  })
+  
+  // console.log('search value', searchInput.value);
+  // const pokeObj = getPokeData(searchInput.value);
+  // console.log('pokemon', pokeObj);
+  // console.log('event', event);
 }
 
 // take input data in the form field and use that to search the API and return the data it finds
