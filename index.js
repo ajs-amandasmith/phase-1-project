75f31850836bd2fb875d943fa5eb025c2906f953
Amanda Smith <ajs.amandasmith@gmail.com>
2022-04-28 21:06:42 -0500

    adding html elements and fetch function

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener('ContentLoad', () => {
+
+})
+
+function getPokeData() {
+  fetch('https://pokeapi.co/api/v2/pokemon/')
+  .then(res => res.json())
+  .then(data => console.log(data))
+}
+
